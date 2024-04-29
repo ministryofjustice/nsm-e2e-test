2da753dec74b1d7595ac8eef2f69f51bed981539
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-04-29 09:37:13 +0100

    Remove firm account number

--- e2e/crm7/pages/provider/your-details.js
+++ e2e/crm7/pages/provider/your-details.js
@@ -7,7 +7,6 @@ export default class YourDetailsPage {
 
     async fillYourDetails() {
         await this.page.getByLabel('Firm name').fill(formData.firmName);
-        await this.page.getByLabel('Firm account number').fill(formData.firmAccountNumber);
         await this.page.getByLabel('Address line 1').fill(formData.addressLine1);
         await this.page.getByLabel('Town or city').fill(formData.townOrCity);
         await this.page.getByLabel('Postcode').fill(formData.postcode);
