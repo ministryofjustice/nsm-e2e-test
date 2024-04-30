61d7335581ff81aced31c3cdfa265cb6a9952ae1
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2024-04-30 14:56:48 +0100

    Adds CRM4 scenario - Prison law matter and a total of less than £100

--- e2e/crm4/provider/not-prison-matter.spec.js
+++ e2e/crm4/provider/not-prison-matter.spec.js
@@ -43,4 +43,33 @@ export default function createTests() {
 		});
 	});
 
+	test(' Start a new application that is a Prison Law Matter and more than £100', async () => {
+		await test.step('Starting a new application', async () => {
+			const yourApplicationsPage = new YourApplicationsPage(page);
+			// Actions
+			await yourApplicationsPage.goto();
+			// Expectations
+			await expect(page.getByRole('button', { name: 'Make a new application' })).toBeVisible();
+			await page.getByRole('button', { name: 'Make a new application' }).click();
+		});
+
+		await test.step('Selecting the type of application', async () => {
+			const isThisPrisonLawPage = new IsThisPrisonLawPage(page);
+			// Actions
+			await isThisPrisonLawPage.fillPrisonLawForm('Yes');
+
+			//Expectations
+			await expect(page.getByRole('heading', { name: 'Are you applying for a total' })).toBeVisible();
+		});
+
+		await test.step('Applying for total of less than £100', async () => {
+			const applyingTotalAmountPage = new ApplyingTotalAmountPage(page);
+			// Actions
+			await applyingTotalAmountPage.fillApplyingTotalAmountForm('Yes');
+			// Expectations
+			await expect(page.getByRole('heading', { name: 'You do not need to apply' })).toBeVisible();
+			await page.getByRole('link', { name: 'Finish' }).click();
+		});
+	})
+
 }
