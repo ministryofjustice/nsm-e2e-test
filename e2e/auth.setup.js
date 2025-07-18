229827a7140f55ee321fa6aa4400b499a4830088
Ellis Kenyő <elken@users.noreply.github.com>
2025-07-18 14:21:20 +0100

    Update authSetup to look for provider name (#218)

--- e2e/auth.setup.js
+++ e2e/auth.setup.js
@@ -1,40 +1,53 @@
-import { test as setup, expect } from '@playwright/test';
-import { authenticateAsCaseworker, authenticateAsProvider, authenticateAsSupervisor, providerAppUrl } from '../helpers/index';
+import { test as setup, expect } from "@playwright/test";
+import {
+  authenticateAsCaseworker,
+  authenticateAsProvider,
+  authenticateAsSupervisor,
+  providerAppUrl,
+} from "../helpers/index";
 
-const supervisorFile = 'playwright/.auth/supervisor.json';
-const caseworkerFile = 'playwright/.auth/caseworker.json';
-const providerFile = 'playwright/.auth/provider.json';
+const supervisorFile = "playwright/.auth/supervisor.json";
+const caseworkerFile = "playwright/.auth/caseworker.json";
+const providerFile = "playwright/.auth/provider.json";
 
-setup('authenticate as provider', async ({ page }) => {
-    await authenticateAsProvider(page);
+setup("authenticate as provider", async ({ page }) => {
+  await authenticateAsProvider(page);
 
-    // Wait until the page reaches a state where all cookies are set.
-    await expect(page.getByText('provider@example.com')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Submit a crime form' })).toBeVisible();
+  // Wait until the page reaches a state where all cookies are set.
+  await expect(
+    page.getByText("Test User").or(page.getByText("provider@example.com")),
+  ).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Submit a crime form" }),
+  ).toBeVisible();
 
-    // End of authentication steps.
+  // End of authentication steps.
 
-    await page.context().storageState({ path: providerFile });
+  await page.context().storageState({ path: providerFile });
 });
 
-setup('authenticate as supervisor', async ({ page }) => {
-    await authenticateAsSupervisor(page);
+setup("authenticate as supervisor", async ({ page }) => {
+  await authenticateAsSupervisor(page);
 
-    // Wait until the page reaches a state where all cookies are set.
-    await expect(page.getByLabel('Menu', { exact: true }).getByText('Super Visor')).toBeVisible();
+  // Wait until the page reaches a state where all cookies are set.
+  await expect(
+    page.getByLabel("Menu", { exact: true }).getByText("Super Visor"),
+  ).toBeVisible();
 
-    // End of authentication steps.
+  // End of authentication steps.
 
-    await page.context().storageState({ path: supervisorFile });
+  await page.context().storageState({ path: supervisorFile });
 });
 
-setup('authenticate as caseworker', async ({ page }) => {
-    await authenticateAsCaseworker(page);
+setup("authenticate as caseworker", async ({ page }) => {
+  await authenticateAsCaseworker(page);
 
-    // Wait until the page reaches a state where all cookies are set.
-    await expect(page.getByLabel('Menu', { exact: true }).getByText('Case Worker')).toBeVisible();
+  // Wait until the page reaches a state where all cookies are set.
+  await expect(
+    page.getByLabel("Menu", { exact: true }).getByText("Case Worker"),
+  ).toBeVisible();
 
-    // End of authentication steps.
+  // End of authentication steps.
 
-    await page.context().storageState({ path: caseworkerFile });
+  await page.context().storageState({ path: caseworkerFile });
 });
