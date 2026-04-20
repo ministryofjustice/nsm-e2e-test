c23589ae69f2e575c2f8ef0ede1e9197e4440099
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-04-20 17:39:06 +0100

    revert last 2 changes

--- playwright.config.js
+++ playwright.config.js
@@ -31,8 +31,7 @@ module.exports = defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
-    screenshot: 'only-on-failure'
+    trace: 'retain-on-failure'
   },
 
   /* Configure projects for major browsers */
